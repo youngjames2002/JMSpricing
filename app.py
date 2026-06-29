858f65fce2763943100988e440138ec4ad969568
James Young <james@jmsmetaltec.com>
2026-06-29 17:09:04 +0100

    early progress

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import psycopg2
 from jinja2 import Environment, FileSystemLoader
-
-templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
@@ -12,5 +13,16 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def home(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
+
+@app.get("/rates")
+async def rates(request: Request):
+    conn = psycopg2.connect(os.getenv("DATABASE_STRING"))
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM materials;")
+    mat_rows = cur.fetchall()
+    cur.execute("SELECT * FROM burden_rates;")
+    rates_rows = cur.fetchall()
+    cur.close()
+    return templates.TemplateResponse("index.html", {"request": request, "materials":mat_rows, "burden_rates": rates_rows})
